36873a6e34517135e29444cdb9d65c84087c6006
syed Rizvi <rizvi5a@yahoo.com>
2021-07-21 05:48:35 -0500

    deploying to heroku

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
   
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
