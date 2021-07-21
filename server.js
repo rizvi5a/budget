323a58a115477021d42cc7924f0b27c221e1ee24
syed Rizvi <rizvi5a@yahoo.com>
2021-07-21 05:08:58 -0500

    deploying to heroku

--- server.js
+++ server.js
@@ -21,6 +21,19 @@ mongoose.connect("mongodb://localhost/budget", {
   useFindAndModify: false
 });
 
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
+
+
+
 // routes
 app.use(require("./routes/api.js"));
 
